61701abcc160f62a7a03afcad552dd1cff58bc15
andredarochasouza <andredarochasouza.12345@gmail.com>
2022-01-15 21:26:07 -0300

    Front-End #01

--- controle-de-despesas/src/main/java/br/com/finance/cdd/controllers/UserController.java
+++ controle-de-despesas/src/main/java/br/com/finance/cdd/controllers/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,27 +16,29 @@ import br.com.finance.cdd.model.entities.User;
 import br.com.finance.cdd.model.repository.UserRepository;
 
 @RestController
-
+@RequestMapping("/")
 public class UserController {
 
 	@Autowired
 	private UserRepository userRepository;
-	
-	
-//	@RequestMapping(value = "/index",method = RequestMethod.GET)
-//	public ModelAndView index(ModelAndView modelAndView) {
-//		modelAndView = new ModelAndView("index");
-//		System.out.println("entrou no /index");
-//		return modelAndView;
-//	}
 
+	@GetMapping("/")
+	public ModelAndView index(ModelAndView modelAndView) {
+		modelAndView = new ModelAndView("index");
+		return modelAndView;
+	}
+	
+	@GetMapping("/userpage")
+	public ModelAndView userPage(ModelAndView modelAndView) {
+		modelAndView = new ModelAndView("userPage");
+		return modelAndView;
+	}
 
 	@GetMapping("/user/{id}")
 	public @ResponseBody User findByIdUser(@PathVariable long id) {
 		return userRepository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("User Not Found By ID: " + id));
 	}
-
 
 	@PostMapping("/salvar")
 	public @ResponseBody User saveUser(@Valid User user) {
--- controle-de-despesas/src/main/java/br/com/finance/cdd/error/RequestNotFoundException.java
+++ controle-de-despesas/src/main/java/br/com/finance/cdd/error/RequestNotFoundException.java
@@ -4,14 +4,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class RequestNotFoundException extends RuntimeException{
+public class RequestNotFoundException extends RuntimeException {
 
-		/**
-	 * 
-	 */
+	/**
+	* 
+	*/
 	private static final long serialVersionUID = 4985653617425536806L;
 
-		public RequestNotFoundException(String message) {
-			super(message);
-		}
+	public RequestNotFoundException(String message) {
+		super(message);
+	}
 }
