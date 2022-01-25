3757989da7e77b6ead60d943fee92ded3dc01f95
andredarochasouza <andredarochasouza.12345@gmail.com>
2022-01-25 14:32:03 -0300

    segundo commit

--- controle-de-despesas/src/main/java/br/com/finance/cdd/controllers/IndexController.java
+++ controle-de-despesas/src/main/java/br/com/finance/cdd/controllers/IndexController.java
@@ -1,9 +1,23 @@
 package br.com.finance.cdd.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.finance.cdd.dto.UserDTO;
 import br.com.finance.cdd.service.UserServices;
 
 @RestController
@@ -13,41 +27,41 @@ public class IndexController {
 	@Autowired
 	private UserServices serviceUser;
 
-//	// Retorna Page de UsersForm (Lista todos os Usuários não deletados)
-//	@GetMapping()
-//	public ResponseEntity<Page<UserForm>> indexPage(
-//			@RequestParam(name = "numPage", required = false) Integer numPage) {
-//		
-//		Pageable page;
-//		if(numPage != null) 
-//			page = PageRequest.of(numPage - 1, 5);
-//		else
-//			page = PageRequest.of(0, 5);
-//
-//		Page<UserForm> usersForm = serviceUser.findAllUserForm(page);
-//		return new ResponseEntity<Page<UserForm>>(usersForm, HttpStatus.ACCEPTED);
-//	}
-//
-//	// Adiciona um novo User
-//	@PostMapping
-//	public ResponseEntity<UserForm> addUser(@Valid @RequestBody UserForm userForm) {
-//		serviceUser.saveUserForm(userForm);
-//		return new ResponseEntity<UserForm>(userForm, HttpStatus.CREATED);
-//	}
-//
-//	// Deleta um User
-//	@DeleteMapping
-//	public ResponseEntity<UserForm> deleteUser(@RequestParam(name = "id", required = true) Long id) {
-//		UserForm userForm = new UserForm(serviceUser.findByIdUser(id));
-//		serviceUser.deleteUserById(id);
-//		return new ResponseEntity<UserForm>(userForm, HttpStatus.OK);
-//	}
-//	
-//	// Alterar um User,
-//	@RequestMapping(method = {RequestMethod.PUT, RequestMethod.PATCH})
-//	public ResponseEntity<UserForm> updateUser(@RequestParam(name = "id" , required = true) Long id, @Valid @RequestBody UserForm userForm){
-//		serviceUser.updateUserForm(id, userForm);
-//		return new ResponseEntity<UserForm>(userForm, HttpStatus.ACCEPTED);
-//	}
+	// Retorna Page de UsersForm (Lista todos os Usuários não deletados)
+	@GetMapping()
+	public ResponseEntity<Page<UserDTO>> indexPage(
+			@RequestParam(name = "numPage", required = false) Integer numPage) {
+		
+		Pageable page;
+		if(numPage != null) 
+			page = PageRequest.of(numPage - 1, 5);
+		else
+			page = PageRequest.of(0, 5);
+
+		Page<UserDTO> usersDTO = serviceUser.findAllUserDTO(page);
+		return new ResponseEntity<Page<UserDTO>>(usersDTO, HttpStatus.ACCEPTED);
+	}
+
+	// Adiciona um novo User
+	@PostMapping
+	public ResponseEntity<UserDTO> addUser(@Valid @RequestBody UserDTO userDTO) {
+		serviceUser.saveUserDTO(userDTO);
+		return new ResponseEntity<UserDTO>(userDTO, HttpStatus.CREATED);
+	}
+
+	// Deleta um User
+	@DeleteMapping
+	public ResponseEntity<UserDTO> deleteUser(@RequestParam(name = "id", required = true) Long id) {
+		UserDTO userDTO = new UserDTO(serviceUser.findByIdUser(id));
+		serviceUser.deleteUserById(id);
+		return new ResponseEntity<UserDTO>(userDTO, HttpStatus.OK);
+	}
+	
+	// Alterar um User,
+	@RequestMapping(method = {RequestMethod.PUT, RequestMethod.PATCH})
+	public ResponseEntity<UserDTO> updateUser(@RequestParam(name = "id" , required = true) Long id, @Valid @RequestBody UserDTO userDTO){
+		serviceUser.updateUserDTO(id, userDTO);
+		return new ResponseEntity<UserDTO>(userDTO, HttpStatus.ACCEPTED);
+	}
 
 }
