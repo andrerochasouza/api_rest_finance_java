f4a0ba23461ce7ae9b1a672a012d392a30ce7e6d
Thiago Furlan <thiagoferraz10@hotmail.com>
2022-02-01 14:41:51 -0300

    Criação da Validação de CPF (Impossibilitando CPF duplicado no banco)

--- controle-de-despesas/src/main/java/br/com/finance/cdd/repository/UserRepository.java
+++ controle-de-despesas/src/main/java/br/com/finance/cdd/repository/UserRepository.java
@@ -3,6 +3,7 @@ package br.com.finance.cdd.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.finance.cdd.model.User;
@@ -12,4 +13,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
 	public Optional<User> findByName(String name);
 
+	@Query(
+			value = "SELECT count(cpf) FROM FINANCE_IO.USERS u WHERE u.cpf = ?1",
+			nativeQuery = true)
+	Integer findCountCpf(String cpf);
 }
